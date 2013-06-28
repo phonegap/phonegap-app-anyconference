ac1e417c970dd810ad4149aa35253224cc1b186b
adeveria <adeveria@gmail.com>
2013-06-28 16:41:21 -0700

    work on pull-to-action

--- assets/www/js/app/components/collectionView.js
+++ assets/www/js/app/components/collectionView.js
@@ -122,13 +122,13 @@ define(function(require, exports, module) {
 			var pageBottom = this.currentPage.el.offsetTop + this.currentPage.el.offsetHeight;
 
 			if( viewBottom > pageBottom ) {
-			    // Fix the page's height
-			    var pageHeight = this.currentPage.el.offsetHeight;
-			    this.currentPage.el.style.height = pageHeight + 'px';
-			    this.listEl.style.height = pageHeight + 'px';
 				this.addToNewPage();
 				this.currentPage.el.appendChild( itemView.el );
 			}
+            // Fix the page's height
+            var pageHeight = this.currentPage.el.offsetHeight;
+            this.currentPage.el.style.height = pageHeight + 'px';
+            this.listEl.style.height = pageHeight + 'px';
 		},
 		
 		transitionToNext: function() {
