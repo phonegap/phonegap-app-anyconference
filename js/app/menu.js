a9ff70c327ac0cfc0e17923008e9542a819f44d7
adeveria <adeveria@gmail.com>
2013-08-16 16:31:50 -0700

    fix session link for menu

--- js/app/menu.js
+++ js/app/menu.js
@@ -304,7 +304,7 @@ define(function(require, exports, module) {
 		        date: eventData.dates[0].date,
 		        location: eventData.location,
 		        */
-		        defaultScheduleLink: 'sessionList/' + this.defaultDayId
+		        defaultScheduleLink: 'sessionCollection/' + this.defaultDayId
 		    };
             return templateValues;
         },
